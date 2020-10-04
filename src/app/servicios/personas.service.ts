117e69afabcdcc2688c1030fb33827e6e33dff82
German Alvaredo <geralvaredo@github.com>
2020-10-04 17:49:34 -0300

    personas Service

--- src/app/servicios/personas.service.ts
+++ src/app/servicios/personas.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
-import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonasService {
 
-  private url = environment.urlPersonas;
+  public url = environment.urlPersonas;
 
   constructor(private http: HttpClient) { }
 
